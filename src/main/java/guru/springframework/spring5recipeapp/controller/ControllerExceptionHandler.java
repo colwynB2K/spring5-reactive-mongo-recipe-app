acf7b9e86a35bfa90a497353a9e924c993c74d28
eersels3764 <ivo.eersels@idewe.be>
2021-04-21 16:56:13 +0200

    WebFlux Controller Advice -------------------------
    	Basically re-implement this:
    
    	@Slf4j
    	@ControllerAdvice
    	public class ControllerExceptionHandler {
    
    	/*    @ExceptionHandler(NumberFormatException.class)
    		@ResponseStatus(HttpStatus.BAD_REQUEST)
    		public ModelAndView handleNumberFormatException(Exception exception) {
    			log.warn("Handling NumberFormatException");
    			log.warn(exception.getMessage());
    			ModelAndView mav = new ModelAndView("400");
    			mav.addObject("exception", exception);
    
    			return mav;
    		}*/
    	}
    
    	where we handle NumberFormatExceptions when we input text in a form input field which expects numbers like 'Cooking Time'.
    
    	If you do this now, you will get a 'org.springframework.web.bind/support.WebExchangeBindException: Validation failed for argument...' and see a blank page.
    	Basically, when you submit the form, Spring will try to match the field values to fields in the RecipeDTO and there cookTimeMins is an Integer and not a String!
    	So you request fails even before it reaches the 'save' method inside RecipeController, so the error happens within the Spring Framework itself!
    
    	So, the solution is similar to the ExceptionHandler inside of a Controller, but here we no longer see a NumberFormatException. It is now wrapped inside a WebExchangeBindException, so we will put that one in the code instead.

--- src/main/java/guru/springframework/spring5recipeapp/controller/ControllerExceptionHandler.java
+++ src/main/java/guru/springframework/spring5recipeapp/controller/ControllerExceptionHandler.java
@@ -2,22 +2,26 @@ package guru.springframework.spring5recipeapp.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-/*    @ExceptionHandler(NumberFormatException.class)
+    public static final String VIEWS_400 = "400";
+
+    @ExceptionHandler(WebExchangeBindException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ModelAndView handleNumberFormatException(Exception exception) {
+    public String handleNumberFormatException(Exception exception, Model model) {
         log.warn("Handling NumberFormatException");
         log.warn(exception.getMessage());
-        ModelAndView mav = new ModelAndView("400");
-        mav.addObject("exception", exception);
 
-        return mav;
-    }*/
+        model.addAttribute("exception", exception);
+
+        return VIEWS_400;
+    }
 }
